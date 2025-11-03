dd4790adb7ae5c99c8ce0cad66ee98a6ef48589d
Thomas Krantz <thomas.krantz@gmail.com>
2025-11-03 15:02:12 +0100

    Added base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  base: '/taste-wars/', // GitHub Pages subpath
   plugins: [vue()],
   resolve: {
     alias: {
